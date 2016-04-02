a8b135513f7dc991e6a0a01cc59047fef08dc8a0
Jorge De Los Santos <jorgeluk13@gmail.com>
2016-04-02 13:19:20 +0200

    Second part of the Workshop: Suggestions.

--- flights/src/test/java/com/scmspain/workshop/suggestion/SuggestUnitTest.java
+++ flights/src/test/java/com/scmspain/workshop/suggestion/SuggestUnitTest.java
@@ -13,57 +13,53 @@ import rx.functions.Func1;
 import rx.observers.TestSubscriber;
 
 public class SuggestUnitTest {
-  Observable<CharSequence> textInputObservable = Observable.create(new Observable.OnSubscribe<CharSequence>() {
-    @Override
-    public void call(Subscriber<? super CharSequence> subscriber) {
-      subscriber.onNext("a");
-      try {
-        Thread.sleep(160);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      subscriber.onNext("ab");
-      subscriber.onNext("abc");
-      subscriber.onNext("abcd");
-      try {
-        Thread.sleep(160);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      subscriber.onNext("abcde");
-      try {
-        Thread.sleep(160);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      subscriber.onNext("abcdef");
+  Observable<CharSequence> textInputObservable =
+      Observable.create(new Observable.OnSubscribe<CharSequence>() {
+        @Override public void call(Subscriber<? super CharSequence> subscriber) {
+          subscriber.onNext("a");
+          try {
+            Thread.sleep(160);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          subscriber.onNext("ab");
+          subscriber.onNext("abc");
+          subscriber.onNext("abcd");
+          try {
+            Thread.sleep(160);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          subscriber.onNext("abcde");
+          try {
+            Thread.sleep(160);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          subscriber.onNext("abcdef");
 
-      subscriber.onCompleted();
-    }
-  });
+          subscriber.onCompleted();
+        }
+      });
 
-  TestSubscriber<CharSequence> charSequenceSubscriber = new TestSubscriber<>(new Observer<CharSequence>() {
-        @Override
-        public void onCompleted() {
+  TestSubscriber<CharSequence> charSequenceSubscriber =
+      new TestSubscriber<>(new Observer<CharSequence>() {
+        @Override public void onCompleted() {
 
         }
 
-        @Override
-        public void onError(Throwable e) {
+        @Override public void onError(Throwable e) {
 
         }
 
-        @Override
-        public void onNext(CharSequence charSequence) {
+        @Override public void onNext(CharSequence charSequence) {
           System.out.println(charSequence);
         }
       });
 
-  @Test
-  public void printAllInput() throws Exception {
+  @Test public void printAllInput() throws Exception {
     System.out.println("printInput");
-    textInputObservable
-        .subscribe(charSequenceSubscriber);
+    textInputObservable.subscribe(charSequenceSubscriber);
 
     List<CharSequence> expected = new ArrayList<>();
     expected.add("a");
@@ -76,12 +72,13 @@ public class SuggestUnitTest {
     charSequenceSubscriber.assertReceivedOnNext(expected);
   }
 
-  @Test
-  public void printAtLeastThreeChars() throws Exception {
+  @Test public void printAtLeastThreeChars() throws Exception {
     System.out.println("printAtLeastThreeChars");
-    textInputObservable
-        //TODO: Try filtering
-        .subscribe(charSequenceSubscriber);
+    textInputObservable.filter(new Func1<CharSequence, Boolean>() {
+      @Override public Boolean call(CharSequence charSequence) {
+        return charSequence.length() >= 3;
+      }
+    }).subscribe(charSequenceSubscriber);
 
     List<CharSequence> expected = new ArrayList<>();
     expected.add("abc");
@@ -92,12 +89,9 @@ public class SuggestUnitTest {
     charSequenceSubscriber.assertReceivedOnNext(expected);
   }
 
-  @Test
-  public void printOnlyWhenPauseFor150Milliseconds() throws Exception {
+  @Test public void printOnlyWhenPauseFor150Milliseconds() throws Exception {
     System.out.println("printAtLeastWhenPause");
-    textInputObservable
-        //TODO: Do you want a tip? try debounce
-        .subscribe(charSequenceSubscriber);
+    textInputObservable.debounce(150, TimeUnit.MILLISECONDS).subscribe(charSequenceSubscriber);
 
     List<CharSequence> expected = new ArrayList<>();
     expected.add("a");
@@ -108,16 +102,12 @@ public class SuggestUnitTest {
     charSequenceSubscriber.assertReceivedOnNext(expected);
   }
 
-  @Test
-  public void printFlatMapRequests() throws Exception {
-    textInputObservable
-        .flatMap(new Func1<CharSequence, Observable<CharSequence>>() {
-          @Override
-          public Observable<CharSequence> call(final CharSequence charSequence) {
-            return request(charSequence);
-          }
-        })
-        .subscribe(charSequenceSubscriber);
+  @Test public void printFlatMapRequests() throws Exception {
+    textInputObservable.switchMap(new Func1<CharSequence, Observable<CharSequence>>() {
+      @Override public Observable<CharSequence> call(final CharSequence charSequence) {
+        return request(charSequence);
+      }
+    }).subscribe(charSequenceSubscriber);
 
     List<CharSequence> expected = new ArrayList<>();
     expected.add("a->fast");
@@ -125,69 +115,55 @@ public class SuggestUnitTest {
     expected.add("abcdef->slooooow");
     charSequenceSubscriber.awaitTerminalEvent();
     charSequenceSubscriber.assertReceivedOnNext(expected);
-    //TODO: IT`S OK? TRY WITH .switchMap()
   }
 
-  Observable<CharSequence> fastRequest = Observable.<CharSequence>just("fast")
-      .delay(10, TimeUnit.MILLISECONDS);
-  Observable<CharSequence> slowRequest = Observable.<CharSequence>just("slooooow")
-      .delay(300, TimeUnit.MILLISECONDS);
+  Observable<CharSequence> fastRequest =
+      Observable.<CharSequence>just("fast").delay(10, TimeUnit.MILLISECONDS);
+  Observable<CharSequence> slowRequest =
+      Observable.<CharSequence>just("slooooow").delay(300, TimeUnit.MILLISECONDS);
 
-  @NonNull
-  private Observable<CharSequence> request(final CharSequence charSequence) {
-    return (charSequence.length()%2==0)?
-        slowRequest
-            .map(new Func1<CharSequence, CharSequence>() {
-              @Override
-              public CharSequence call(CharSequence speed) {
-                return charSequence + "->" + speed;
-              }
-            })
-        : fastRequest
-            .map(new Func1<CharSequence, CharSequence>() {
-              @Override
-              public CharSequence call(CharSequence speed) {
-                return charSequence + "->" + speed;
-              }
-            });
-  }
-
-
-  @Test
-  public void testWithExternalRequests() throws Exception {
-    final SuggestionBusiness suggestionBusiness = new SuggestionBusiness();
-    TestSubscriber<List<String>> testSubscriber = new TestSubscriber<>(new Observer<List<String>>() {
-      @Override
-      public void onCompleted() {
-
-      }
-
-      @Override
-      public void onError(Throwable e) {
-
-      }
-
-      @Override
-      public void onNext(List<String> strings) {
-        for (String string : strings) {
-          System.out.println(string);
-        }
+  @NonNull private Observable<CharSequence> request(final CharSequence charSequence) {
+    return (charSequence.length() % 2 == 0) ? slowRequest.map(
+        new Func1<CharSequence, CharSequence>() {
+          @Override public CharSequence call(CharSequence speed) {
+            return charSequence + "->" + speed;
+          }
+        }) : fastRequest.map(new Func1<CharSequence, CharSequence>() {
+      @Override public CharSequence call(CharSequence speed) {
+        return charSequence + "->" + speed;
       }
     });
+  }
 
-    Observable.<CharSequence>just("Barc")
-        .switchMap(new Func1<CharSequence, Observable<List<String>>>() {
-          @Override
-          public Observable<List<String>> call(CharSequence charSequence) {
+  @Test public void testWithExternalRequests() throws Exception {
+    final SuggestionBusiness suggestionBusiness = new SuggestionBusiness();
+    TestSubscriber<List<String>> testSubscriber =
+        new TestSubscriber<>(new Observer<List<String>>() {
+          @Override public void onCompleted() {
+
+          }
+
+          @Override public void onError(Throwable e) {
+
+          }
+
+          @Override public void onNext(List<String> strings) {
+            for (String string : strings) {
+              System.out.println(string);
+            }
+          }
+        });
+
+    Observable.<CharSequence>just("Barc").switchMap(
+        new Func1<CharSequence, Observable<List<String>>>() {
+          @Override public Observable<List<String>> call(CharSequence charSequence) {
             return suggestionBusiness.requestSuggestions(charSequence);
           }
-        })
-        .subscribe(testSubscriber);
+        }).subscribe(testSubscriber);
     testSubscriber.awaitTerminalEvent();
   }
 
-  @Test @Ignore
-  public void exampleInApp() throws Exception {
+  @Test @Ignore public void exampleInApp() throws Exception {
 /*
     Observable.just(Collections.singletonList("Barcelona"))
         .subscribe(new Action1<List<String>>() {
